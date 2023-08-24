0fea11312376849ce2588451f9a906314b7839bd
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-08-24 13:53:15 -0300

    refact: extrair apenas o que precisamos dos detalhes do pokemons

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -1,7 +1,25 @@
 const pokeApi = {};
 
+function convertPokeApiDetailsToPokemon(pokemonDetails) {
+  const pokemon = new Pokemon();
+  pokemon.name = pokemonDetails.name;
+  pokemon.number = pokemonDetails.order;
+
+  const types = pokemonDetails.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
+
+  pokemon.types = types;
+  pokemon.type = type;
+
+  pokemon.photo = pokemonDetails.sprites.other.dream_world.front_default;
+
+  return pokemon;
+}
+
 pokeApi.getPokemonDetails = (pokemon) => {
-  return fetch(pokemon.url).then((response) => response.json());
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailsToPokemon);
 };
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
