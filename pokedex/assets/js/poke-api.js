66d7eef2288cb26a32a0b9f5db94df687cdff51c
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-08-24 13:27:54 -0300

    feat: buscar detalhes dos pokemons

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -1,10 +1,17 @@
 const pokeApi = {};
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemonDetails = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json());
+};
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
   return fetch(url)
     .then((response) => response.json())
     .then((body) => body.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetails))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
     .catch((error) => console.error(error));
 };
