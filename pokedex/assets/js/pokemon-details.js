8b3d35ee9f486ba6b6a487832783ab3a3207a304
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-08-28 14:36:55 -0300

    feat: added tela com os detalhes do pokemon

--- pokedex/assets/js/pokemon-details.js
+++ pokedex/assets/js/pokemon-details.js
@@ -1,6 +1,7 @@
 const favoriteButton = document.getElementById("favorite-button-js");
 const pokemonInfo = document.getElementById("info-js");
 const body = document.getElementById("body-js");
+const details = document.getElementById('details-js');
 
 const key = "pokemon";
 const pokemonName = new URLSearchParams(window.location.search).get(key);
@@ -13,13 +14,25 @@ pokeApi.getPokemonByName(pokemonName).then((pokemonDetails) => {
   generateHTML(pokemonDetails);
 });
 
+/**
+ * Gera o HTML dos detalhes do Pokemon.
+ * 
+ * @param {*} pokemonDetails que deve ser do tipo PokemonDetails.
+ */
 function generateHTML(pokemonDetails) {
   const basicInfo = generateBasicInfo(pokemonDetails.summary);
 
   pokemonInfo.innerHTML = basicInfo;
   body.className = `${pokemonDetails.summary.type}`;
+
+  details.innerHTML = generateAboutInfo(pokemonDetails.about);
 }
 
+/**
+ * Gera o HTML do resumo do Pokemon.
+ * 
+ * @param {*} summary resumo do Pokemon.
+ */
 function generateBasicInfo(summary) {
   return `
     <div class="info__basic">
@@ -43,5 +56,34 @@ function generateBasicInfo(summary) {
       src="${summary.photo}"
       alt="${summary.name} image"
     />
+  `;
+}
+
+/**
+ * Gera o HTML da parte 'sobre' do Pokemon.
+ * @param {*} about informações 'sobre' o Pokemon.
+ */
+function generateAboutInfo(about) {
+  return `
+    <table class="details__about">
+      <tbody>
+        <tr>
+          <th>Species</th>
+          <td>${about.species}</td>
+        </tr>
+        <tr>
+          <th>Height</th>
+          <td>${about.height * 10} cm</td>
+        </tr>
+        <tr>
+          <th>Weight</th>
+          <td>${about.weight / 10} Kg</td>
+        </tr>
+        <tr>
+          <th>Abilities</th>
+          <td>${about.abilities.join(', ')}</td>
+        </tr>
+      </tbody>
+    </table>
   `;
 }
