4b380871aa06068d545d58721013300adbafb017
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-08-28 08:58:33 -0300

    feat: gerar as informações básicas do pokemon

--- pokedex/assets/js/pokemon-details.js
+++ pokedex/assets/js/pokemon-details.js
@@ -1,4 +1,6 @@
 const favoriteButton = document.getElementById("favorite-button-js");
+const pokemonInfo = document.getElementById("info-js");
+const body = document.getElementById("body-js");
 
 const key = "pokemon";
 const pokemonName = new URLSearchParams(window.location.search).get(key);
@@ -8,5 +10,38 @@ favoriteButton.addEventListener("click", () => {
 });
 
 pokeApi.getPokemonByName(pokemonName).then((pokemonDetails) => {
-  console.log(pokemonDetails);
+  generateHTML(pokemonDetails);
 });
+
+function generateHTML(pokemonDetails) {
+  const basicInfo = generateBasicInfo(pokemonDetails.summary);
+
+  pokemonInfo.innerHTML = basicInfo;
+  body.className = `${pokemonDetails.summary.type}`;
+}
+
+function generateBasicInfo(summary) {
+  return `
+    <div class="info__basic">
+      <div class="info__basic__name-type">
+        <span class="info__basic__name">${summary.name}</span>
+        <ol class="info__basic__type-list">
+          ${summary.types
+            .map(
+              (type) =>
+                `<li class="info__basic__type-list__item ${type}">${type}</li>`
+            )
+            .join("")}
+        </ol>
+      </div>
+
+      <span class="info__basic__number">#${summary.number}</span>
+    </div>
+
+    <img
+      class="info__img"
+      src="${summary.photo}"
+      alt="${summary.name} image"
+    />
+  `;
+}
